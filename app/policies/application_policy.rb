c7e5427a5fc162cf766b78a06644de74a2a64dc7
Jocelyn Ashwell <jocelynashwell@gmail.com>
2015-02-18 20:22:42 -0500

    Fixing bugs

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -6,8 +6,8 @@ class ApplicationPolicy
     @record = record
   end
 
-  def index
-    true
+  def index?
+    false
   end
 
   def show?
@@ -35,7 +35,7 @@ class ApplicationPolicy
   end
 
   def scope
-    record.class
+    Pundit.policy_scope!(user, record.class)
   end
 
   class Scope
